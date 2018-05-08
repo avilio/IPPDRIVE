8269bb46ccb72ca7d2712b97d92bdbffe9c514ed
Claudio Almeida <cmma89@gmail.com>
2018-05-08 21:14:55 +0100

    progresso no pedido POST mas sem resposta esperada no request.dart

--- lib/RequestsAPI/requests.dart
+++ lib/RequestsAPI/requests.dart
@@ -1,11 +1,24 @@
 import 'dart:async';
+import 'dart:convert';
 
 import 'package:http/http.dart' as http;
 
 
 Future<String> postRequest () async {
-//TODO tratar dos comandos de post para fazer o pedido
-return null;
-}
 
-//TODO Tratar do json de resposta
+  var url ='https://pae.ipportalegre.pt/testes2/wsjson/api/app/ws-authenticate';
+  var body = { "data": { "apikey": "12345678901234567890" } };
+
+  http.post(url,
+      headers: {'Content-type': 'application/json'},
+      body: jsonEncode(body)
+  ).then((http.Response response) {
+    print("Response status: ${response.statusCode}");
+    print("Response body: ${response.body}");
+    print(response.headers);
+    print(response.request);
+
+  });
+
+  return null;
+}
