c1a94d2a8480ce503665eec5fc2250b51bd29023
Claudio Almeida Portatil <cmma89@gmail.com>
2018-05-08 15:29:28 +0100

    Adicionado o package de http, e criaçao da do request.dart

---
+++ lib/RequestsAPI/requests.dart
@@ -0,0 +1,11 @@
+import 'dart:async';
+
+import 'package:http/http.dart' as http;
+
+
+Future<String> postRequest () async {
+//TODO tratar dos comandos de post para fazer o pedido
+return null;
+}
+
+//TODO Tratar do json de resposta
