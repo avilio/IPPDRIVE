da14ce06894c72dbcedd882ea18356b504fedb84
Claudio Almeida <cmma89@gmail.com>
2018-05-23 13:20:36 +0100

    expansion tile feito dos semestres mas falta ser recursivo caso o conteudo seja directory

--- lib/pages/homePage.dart
+++ lib/pages/homePage.dart
@@ -37,12 +37,120 @@ class ListFolderState extends State<ListFolder> {
           style: new TextStyle(fontWeight: FontWeight.bold),
         ),
       ),
-      body: new Center(
-        child: semestreUm(list, session, req),
-      ),
+      body: semestres(list, session, req)
+            //new Divider(height: 10.0, color: cAppBlue,),
+
     );
   }
 }
+
+Widget semestres(list,session,req) {
+
+  List sem1 = new List();
+  List sem2 = new List();
+  sem1 = semestreUm(list, session, req);
+  sem2 = semestreDois(list, session, req);
+
+  return new ListView.builder(
+      itemCount: 1,
+      itemBuilder: (BuildContext context, int index) {
+        return Column(
+          children: <Widget>[
+            new Container(
+              decoration:new BoxDecoration(
+                shape: BoxShape.rectangle,
+                //borderRadius: new BorderRadius.circular(8.0),
+              border: Border.all(style: BorderStyle.solid, color: cAppBlackish)),
+              child: new ExpansionTile(
+                //leading: new CircleAvatar(backgroundColor: cAppBlueAccent,backgroundImage: AssetImage("assets/images/icon.png"),),
+                title:new Text("Semestre 1",textScaleFactor: 1.5,),
+                children: sem1.map((val) => new ListTile(
+                  title: Container(
+                    child: new Text(val['title'].toString().split('-')[0],textScaleFactor: 0.95,)
+                  ),
+                )).toList(),
+              ),
+            ),
+            new Container(
+              decoration:new BoxDecoration(
+                  shape: BoxShape.rectangle,
+                  //borderRadius: new BorderRadius.circular(8.0),
+                  border: Border.all(style: BorderStyle.solid, color: cAppBlackish)),
+              child: new ExpansionTile(
+                //leading: new CircleAvatar(backgroundColor: cAppBlueAccent,backgroundImage: AssetImage("assets/images/icon.png"),),
+                title:new Text("Semestre 2",textScaleFactor: 1.5,),
+                children: sem2.map((val) => new ListTile(
+                  title: Container(
+                      child: new Text(val['title'].toString().split('-')[0],textScaleFactor: 0.95,)
+                  ),
+                )).toList(),
+              ),
+            ),
+          ],
+        );
+      });
+}
+
+List semestreUm(list,session,req) {
+
+  List sem1 = new List();
+
+  for (var i = 0; i < list.length; ++i) {
+    if(list[i]['pathParent'].contains('Semestre1'))
+      sem1.add(list[i]);
+  }
+
+  return sem1;
+/*
+  return new ListView.builder(
+      itemCount: sem1.length,
+      itemBuilder: (BuildContext context, int index) {
+        return new ExpansionTile(
+              title: new GestureDetector(
+                child: new Text('${sem1[index]['title'].toString().split('-')[0]}'),
+                onTap: () async {
+                  Map insideFolder = await req.courseUnitsContents(sem1[index]['id'],session);
+                  new Expanded(child: new Text('${insideFolder['response']['childs'][index]['title']}'),);
+                },
+              ),
+            /*  onTap: () async {
+                Map insideFolder = await req.courseUnitsContents(sem1[index]['id'],session);
+                //Expanded(child: new Text('${insideFolder['response']['childs'][index]['title']}'),);
+              },*/
+            );
+           // new Divider(height: 10.0, color: cAppBlue,),
+        //new Text('${sem2[index]}'),
+      });*/
+}
+List semestreDois(list,session,req) {
+
+  List sem2 = new List();
+
+  for (var i = 0; i < list.length; ++i) {
+    if(list[i]['pathParent'].contains('Semestre2'))
+      sem2.add(list[i]);
+  }
+
+  return sem2;
+/*
+  return new ListView.builder(
+      itemCount: sem2.length,
+      itemBuilder: (BuildContext context, int index) {
+        return new InkWell(
+              child: new Text('${sem2[index]['title'].toString().split('-')[0]}'),
+              onTap: () async {
+                Map insideFolder = await req.courseUnitsContents(sem2[index]['id'],session);
+                //Expanded(child: new Text('${insideFolder['response']['childs'][index]['title']}'),);
+              },
+              onDoubleTap: null,
+              onLongPress: null,
+              onTapDown: null,
+            );
+           // new Divider(height: 10.0, color: cAppBlue,),
+            //new Text('${sem2[index]}'),
+      });*/
+}
+/*
 
 Widget semestreUm(list,session,req) {
 
@@ -75,26 +183,27 @@ Widget semestreUm(list,session,req) {
         );
       });
 }
-
+*/
+/*
 Widget semestreDois(list,session,req) {
 
-  List sem1 = new List();
+  List sem2 = new List();
 
   for (var i = 0; i < list.length; ++i) {
     if(list[i]['pathParent'].contains('Semestre2'))
-      sem1.add(list[i]);
+      sem2.add(list[i]);
   }
 
   return new ListView.builder(
-      itemCount: sem1.length,
+      itemCount: sem2.length,
       itemBuilder: (BuildContext context, int index) {
         return new Column(
           children: <Widget>[
             new Padding(padding: new EdgeInsets.all(10.0)),
             new InkWell(
-              child: new Text('${sem1[index]['title'].toString().split('-')[0]}'),
+              child: new Text('${sem2[index]['title'].toString().split('-')[0]}'),
               onTap: () async {
-                Map insideFolder = await req.courseUnitsContents(sem1[index]['id'],session);
+                Map insideFolder = await req.courseUnitsContents(sem2[index]['id'],session);
                 //Expanded(child: new Text('${insideFolder['response']['childs'][index]['title']}'),);
               },
               onDoubleTap: null,
@@ -106,4 +215,4 @@ Widget semestreDois(list,session,req) {
           ],
         );
       });
-}
+}*/
