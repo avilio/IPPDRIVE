3308a18b96634f4a8f314bd88923ba9f8e460631
Cláudio Almeida <cmma89@gmail.com>
2018-06-11 23:46:06 +0100

    api class feita

---
+++ lib/api.dart
@@ -0,0 +1,35 @@
+import 'dart:io';
+import 'dart:async';
+import 'dart:convert';
+
+class API {
+
+  ///creates a singleton
+  static final API _instance = new API.internal();
+  API.internal();
+  factory API() => _instance;
+
+  Future<dynamic> post(String url, Map jsonMap) async {
+    var result;
+    HttpClient httpClient = new HttpClient();
+
+    HttpClientRequest request = await httpClient.postUrl(Uri.parse(url));
+    request.headers.set('content-type', 'application/json');
+    request.add(utf8.encode(json.encode(jsonMap)));
+    HttpClientResponse response = await request.close();
+
+    if (response.statusCode == HttpStatus.OK) {
+      result = jsonDecode(await response.transform(utf8.decoder).join());
+    } else
+      result = new Exception('Erro ${response.statusCode}');//to string se nao funcionar
+
+    httpClient.close();
+    request.close();
+
+    return result;
+  }
+
+
+  //todo get para ficheiro
+
+}
